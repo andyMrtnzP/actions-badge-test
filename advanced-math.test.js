c183bc7d16a31b09862db2c04849c85ddf81c24e
Anderson Martínez <anderson@browserless.io>
2023-11-03 10:17:01 -0400

    feat: subtraction

--- advanced-math.test.js
+++ advanced-math.test.js
@@ -6,3 +6,10 @@ test('adds 1 to any number', () => {
   expect(math.addOne(-1)).toBe(0);
   expect(math.addOne(0)).toBe(1);
 });
+
+test('substract 1 to any number', () => {
+  expect(math.takeOne(2)).toBe(1);
+  expect(math.takeOne(4)).toBe(3);
+  expect(math.takeOne(-1)).toBe(-2);
+  expect(math.takeOne(0)).toBe(-1);
+});
